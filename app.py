310467a5234cd9cace1a74f7e5d0cb2d075f63bf
Notebook <bortolozogui@gmail.com>
2024-08-16 12:37:36 -0300

    primeira semana

---
+++ app.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input(''))
+    b = int(input(''))
+    c = int(input(''))
+    d = int(input(''))
+
+    ab = a ** b
+    cd = c ** d
+
+    res = ab + cd
+
+    print (res)
